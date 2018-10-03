8cd62910be32b6a39b3fdbc0591445c9da6606e1
Nguyen Hieu <hieu.nt@workhouse.me>
2018-10-03 14:43:08 +0700

    Add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import NavBar from './components/layout/NavBar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar />
-        <Landing />
-        <Footer />
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Footer />
       </div>
+      </Router>
+      
     );
   }
 }
